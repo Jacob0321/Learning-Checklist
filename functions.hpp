e105e0363bfad3b1d1319e3a17a2ccc041bd593a
Jacob0321 <77897102+Jacob0321@users.noreply.github.com>
2023-04-28 22:23:53 -0400

    Update functions.hpp
    Added Search Functionality

--- functions.hpp
+++ functions.hpp
@@ -4,14 +4,33 @@
 #include <string>
 #include <vector>
 
-	std::cout << "1. Add a task" << std::endl;
-	std::cout << "2. Remove a task" << std::endl;
+
+std::vector<std::string> read_checklist_file(const std::string& filename) {
+    std::vector<std::string> checklist;
+    std::ifstream infile(filename);
+    std::string task;
+    while (std::getline(infile, task)) {
+        checklist.push_back(task);
+    }
+    return checklist;
+}
+
+void print_checklist(const std::vector<std::string>& checklist) {
+    int numChoice = 1;
+    for (const auto& task : checklist) {
+        std::cout << numChoice << ". " << task << std::endl;
+        numChoice++;
+    }
+    std::cout << std::endl << std::endl << "What would you like to do?" << std::endl;
+    std::cout << "1. Add a task" << std::endl;
+    std::cout << "2. Remove a task" << std::endl;
+    std::cout << "3. Search for a task" << std::endl;
 }
 
 int get_user_choice() {
     int choice;
     std::cin >> choice;
-    while (choice != 1 && choice != 2) {
+    while (choice != 1 && choice != 2 && choice != 3) {
         std::cout << "Invalid choice" << std::endl;
         std::cin >> choice;
     }
@@ -20,6 +39,7 @@ int get_user_choice() {
 
 void handle_user_choice(int choice, std::vector<std::string>& checklist) {
     std::string task; // Initialize task here
+    bool found = false;
     switch (choice) {
     case 1:
         std::cout << "Enter a task:" << std::endl;
@@ -40,6 +60,20 @@ void handle_user_choice(int choice, std::vector<std::string>& checklist) {
             std::cout << "Task removed" << std::endl;
         }
         break;
+    case 3:
+        std::cout << "Enter a search query:" << std::endl;
+        std::cin.ignore();
+        std::getline(std::cin, task);
+        for (int i = 0; i < checklist.size(); i++) {
+            if (checklist[i].find(task) != std::string::npos) {
+                std::cout << i + 1 << ". " << checklist[i] << std::endl;
+                found = true;
+            }
+        }
+        if (!found) {
+            std::cout << "No tasks found" << std::endl;
+        }
+		break;
     default:
         std::cout << "Invalid choice" << std::endl;
         break;
